d45f3b7bc31c11dab468bc58b5347573ceb86aae
winffee <xiaxucheng@126.com>
2017-09-28 14:19:28 +0800

    Add project files.

---
+++ SplashScreenDemo/SplashScreenDemo.Android/SplashActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.App;
+using System.Threading.Tasks;
+
+namespace SplashScreenDemo.Droid
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        static readonly string TAG = "X:" + typeof(SplashActivity).Name;
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+        
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+        
+        async void SimulateStartup()
+        {
+            await Task.Delay(8000);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
